124a2283c071ec9cda6a24576add8eda8953af9a
faritg1 <113639494+faritg1@users.noreply.github.com>
2023-11-07 12:32:02 -0500

    feat: :sparkles: Implement entities and data dbfirst

---
+++ Persistence/Data/ProyectoContext.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Entities;
+
+namespace Persistence.Data;
+
+public partial class ProyectoContext : DbContext
+{
+    public ProyectoContext()
+    {
+    }
+
+    public ProyectoContext(DbContextOptions<ProyectoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<PersonType> PersonTypes { get; set; }
+
+    public virtual DbSet<State> States { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;user=root;password=123456;database=production", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<City>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("city");
+
+            entity.HasIndex(e => e.IdstateFk, "FK_city_state_IdstateFk");
+
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("name");
+
+            entity.HasOne(d => d.IdstateFkNavigation).WithMany(p => p.Cities)
+                .HasForeignKey(d => d.IdstateFk)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        });
+
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("country");
+
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("customer");
+
+            entity.HasIndex(e => e.IdcityFk, "FK_customer_city_IdcityFk");
+
+            entity.HasIndex(e => e.IdTipoPersonaFk, "FK_customer_person_type_IdTipoPersonaFk");
+
+            entity.HasIndex(e => e.Idcustomer, "UC_Idcustomer").IsUnique();
+
+            entity.Property(e => e.DateRegister).HasColumnName("date_register");
+            entity.Property(e => e.Idcustomer)
+                .IsRequired()
+                .HasMaxLength(20);
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("name");
+
+            entity.HasOne(d => d.IdTipoPersonaFkNavigation).WithMany(p => p.Customers)
+                .HasForeignKey(d => d.IdTipoPersonaFk)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            entity.HasOne(d => d.IdcityFkNavigation).WithMany(p => p.Customers)
+                .HasForeignKey(d => d.IdcityFk)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        });
+
+        modelBuilder.Entity<PersonType>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("person_type");
+
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<State>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("state");
+
+            entity.HasIndex(e => e.IdcountryFk, "FK_state_country_IdcountryFk");
+
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnName("name");
+
+            entity.HasOne(d => d.IdcountryFkNavigation).WithMany(p => p.States)
+                .HasForeignKey(d => d.IdcountryFk)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
