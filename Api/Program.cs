1f3a1fc53ff2b5ef793af2ccf37bb565d293db79
faritg1 <113639494+faritg1@users.noreply.github.com>
2023-11-07 13:20:17 -0500

    feat: :arrow_up: Implement dependencies migration

--- Api/Program.cs
+++ Api/Program.cs
@@ -25,6 +25,23 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+    try
+    {
+        var context = services.GetRequiredService<ProyectoContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var _logger = loggerFactory.CreateLogger<Program>();
+        _logger.LogError(ex, "Ocurrio un error durante la migracion");
+    }
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
