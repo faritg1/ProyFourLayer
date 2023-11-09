99e242de08565f6e4aaa9ae9009a16f8bd0c7706
faritg1 <113639494+faritg1@users.noreply.github.com>
2023-11-09 12:57:38 -0500

    feat: :sparkles: Implement mapping

---
+++ Api/Profiles/MappingProfiles .cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Api.Profiles;
+public class MappingProfiles : Profile
+{
+    public MappingProfiles ()
+    {
+        CreateMap<City, CityDto>().ReverseMap();
+        CreateMap<Country, CountryDto>().ReverseMap();
+        CreateMap<Customer, CustomerDto>().ReverseMap();
+        CreateMap<PersonType, PersonTypeDto>().ReverseMap();
+        CreateMap<State, StateDto>().ReverseMap();
+    }
+}
