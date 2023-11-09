0daea41587c4438fd8c804446bd7e16e92120a67
faritg1 <113639494+faritg1@users.noreply.github.com>
2023-11-09 11:11:22 -0500

    feat: :hammer: Change of identifier in the baseentity

--- Domain/Entities/PersonType.cs
+++ Domain/Entities/PersonType.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 
 namespace Domain.Entities;
 
-public partial class PersonType
+public partial class PersonType : BaseEntity
 {
-    public int Id { get; set; }
-
     public string Name { get; set; }
 
     public virtual ICollection<Customer> Customers { get; set; } = new List<Customer>();
